0e9a1f413621721474691469edcd9ee924e72b17
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2023-11-05 12:01:48 +0200

    the second commit

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
 'use strict';
-/*const movements= [200, 450, -400, 3000, -650, -130, 70, 1300];
-const withdrawals = movements.filter(function(mov){
-    return mov < 0;
-});
-console.log(withdrawals);*/
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
 
-const dogAge1 = [5, 2, 4, 1, 15, 8, 3];
-const dogAge2 = [16, 6, 10, 5, 6, 1, 4];
-function calcAverageHumanAge (ages) {
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
 
- /*const humanAges = ages.map(age =>
-    (age <=2 ? 2* age: 16 + age *4));
-const adults = humanAges.filter(age => age >= 18);
-const  average = adults.reduce( (acc, age) => acc + age,0)/adults.length;
-return average;*/
-     let humanAge = [];
-     for (const age of ages){
-        if (age <= 2){
-            const dogToHumanAge = 2* age;
-            humanAge.push(dogToHumanAge);
-        }
-        else {
-            const dogToHumanAge = 16 + age *4;
-            humanAge.push(dogToHumanAge);
-        }
-     };
-     const filteredHumanAge = humanAge.filter(function(manAge){
-        return manAge >= 18;
-      });
-   const AverageHumanAge = filteredHumanAge.reduce(function(acc , mansAge){
-return acc + mansAge;
-   }, 0)/filteredHumanAge.length;
-   return AverageHumanAge;
-}
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
 
-console.log(calcAverageHumanAge(dogAge1));
-console.log(calcAverageHumanAge(dogAge2));
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+/////////////////////////////////////////////////
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+containerMovements.innerHTML = '';
+
+ function displayMovements (movements){
+  movements.forEach(function (mov, i){
+    const type = mov >0 ? 'deposit':'withdrawal';
+    const html = ` <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">${mov} </div>
+    </div>`;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
+
+
+
